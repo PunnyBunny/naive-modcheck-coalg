dddc66943ee759c04e16af332ca90980a7b005e1
PunnyBunny <sunnycheungabc@gmail.com>
2026-02-25 15:17:38 +0000

    more tests + OUnit

--- lib/checker/naive_modcheck_coalg_checker.ml
+++ lib/checker/naive_modcheck_coalg_checker.ml
@@ -3,4 +3,5 @@ open Naive_modcheck_coalg_logics
 
 module Checkers = struct
   module Relational = Checker.Make (Logics.Relational)
+  module Probabilistic = Checker.Make (Logics.Probabilistic)
 end
--- test/test_naive_modcheck_coalg.ml
+++ test/test_naive_modcheck_coalg.ml
@@ -1,4 +1,5 @@
 open! Core
+open OUnit2
 module Model = Naive_modcheck_coalg_parsers.Model
 module Formula = Naive_modcheck_coalg_parsers.Formula
 module Logics = Naive_modcheck_coalg_logics.Logics
@@ -7,226 +8,191 @@ module Logics = Naive_modcheck_coalg_logics.Logics
 (* Test helpers *)
 (* ============================================================================ *)
 
+let assert_sexp ~expected actual =
+  assert_equal ~cmp:Sexp.equal ~printer:Sexp.to_string_hum
+    (Sexp.of_string expected) actual
+
 let rel_formula s =
-  Formula.parse_relational_formula s
-  |> [%sexp_of: Formula.Ast.Relational_ast.t] |> print_s
+  Formula.parse_relational_formula s |> [%sexp_of: Formula.Ast.Relational_ast.t]
 
 let prob_formula s =
   Formula.parse_probabilistic_formula s
-  |> [%sexp_of: Formula.Ast.Probabilistic_ast.t] |> print_s
+  |> [%sexp_of: Formula.Ast.Probabilistic_ast.t]
 
 let rel_model s =
-  Model.parse_relational_model s
-  |> [%sexp_of: Model.Ast.Relational_ast.t] |> print_s
+  Model.parse_relational_model s |> [%sexp_of: Model.Ast.Relational_ast.t]
 
 let prob_model s =
-  Model.parse_probabilistic_model s
-  |> [%sexp_of: Model.Ast.Probabilistic_ast.t] |> print_s
-
-let logics_rel_formula s =
-  Logics.Relational.parse_formula s
-  |> [%sexp_of: Logics.Relational.Formula_ast.t] |> print_s
-
-let logics_rel_model s =
-  Logics.Relational.parse_model s
-  |> [%sexp_of: Logics.Relational.Model.t] |> print_s
-
-let logics_prob_formula s =
-  Logics.Probabilistic.parse_formula s
-  |> [%sexp_of: Logics.Probabilistic.Formula_ast.t] |> print_s
-
-let logics_prob_model s =
-  Logics.Probabilistic.parse_model s
-  |> [%sexp_of: Logics.Probabilistic.Model.t] |> print_s
+  Model.parse_probabilistic_model s |> [%sexp_of: Model.Ast.Probabilistic_ast.t]
 
 let logics_prob_to_nnf s =
   Logics.Probabilistic.parse_formula s
   |> Logics.Probabilistic.formula_of_ast
-  |> [%sexp_of: Logics.Probabilistic.Formula.t] |> print_s
+  |> [%sexp_of: Logics.Probabilistic.Formula.t]
 
 (* ============================================================================ *)
 (* Relational Formulas *)
 (* ============================================================================ *)
 
-let%expect_test "relational formula: literals" =
-  rel_formula "true";
-  [%expect {| True |}];
-  rel_formula "false";
-  [%expect {| False |}];
-  rel_formula "p";
-  [%expect {| (Ap p) |}];
-  rel_formula "X";
-  [%expect {| (Ap X) |}]
+let test_rel_formula_literals _ =
+  assert_sexp ~expected:"True" (rel_formula "true");
+  assert_sexp ~expected:"False" (rel_formula "false");
+  assert_sexp ~expected:"(Ap p)" (rel_formula "p");
+  assert_sexp ~expected:"(Ap X)" (rel_formula "X")
 
-let%expect_test "relational formula: negation" =
-  rel_formula "~p";
-  [%expect {| (Not (Ap p)) |}]
+let test_rel_formula_negation _ =
+  assert_sexp ~expected:"(Not (Ap p))" (rel_formula "~p")
 
-let%expect_test "relational formula: conjunction" =
-  rel_formula "p && q";
-  [%expect {| (And (Ap p) (Ap q)) |}];
-  rel_formula "p ∧ q";
-  [%expect {| (And (Ap p) (Ap q)) |}]
+let test_rel_formula_conjunction _ =
+  assert_sexp ~expected:"(And (Ap p) (Ap q))" (rel_formula "p && q");
+  assert_sexp ~expected:"(And (Ap p) (Ap q))" (rel_formula "p ∧ q")
 
-let%expect_test "relational formula: disjunction" =
-  rel_formula "p || q";
-  [%expect {| (Or (Ap p) (Ap q)) |}];
-  rel_formula "p ∨ q";
-  [%expect {| (Or (Ap p) (Ap q)) |}]
+let test_rel_formula_disjunction _ =
+  assert_sexp ~expected:"(Or (Ap p) (Ap q))" (rel_formula "p || q");
+  assert_sexp ~expected:"(Or (Ap p) (Ap q))" (rel_formula "p ∨ q")
 
-let%expect_test "relational formula: modalities" =
-  rel_formula "<a>p";
-  [%expect {| (Diamond a () (Ap p)) |}];
-  rel_formula "[a]p";
-  [%expect {| (Box a () (Ap p)) |}]
+let test_rel_formula_modalities _ =
+  assert_sexp ~expected:"(Diamond a () (Ap p))" (rel_formula "<a>p");
+  assert_sexp ~expected:"(Box a () (Ap p))" (rel_formula "[a]p")
 
-let%expect_test "relational formula: fixpoints" =
-  rel_formula "mu X. p || <a>X";
-  [%expect {| (Mu X (Or (Ap p) (Diamond a () (Ap X)))) |}];
-  rel_formula "μ X. p ∨ <a>X";
-  [%expect {| (Mu X (Or (Ap p) (Diamond a () (Ap X)))) |}];
-  rel_formula "nu X. p && [a]X";
-  [%expect {| (Nu X (And (Ap p) (Box a () (Ap X)))) |}];
-  rel_formula "ν X. p ∧ [a]X";
-  [%expect {| (Nu X (And (Ap p) (Box a () (Ap X)))) |}]
+let test_rel_formula_fixpoints _ =
+  assert_sexp ~expected:"(Mu X (Or (Ap p) (Diamond a () (Var X))))"
+    (rel_formula "mu X. p || <a>X");
+  assert_sexp ~expected:"(Mu X (Or (Ap p) (Diamond a () (Var X))))"
+    (rel_formula "μ X. p ∨ <a>X");
+  assert_sexp ~expected:"(Nu X (And (Ap p) (Box a () (Var X))))"
+    (rel_formula "nu X. p && [a]X");
+  assert_sexp ~expected:"(Nu X (And (Ap p) (Box a () (Var X))))"
+    (rel_formula "ν X. p ∧ [a]X")
 
-let%expect_test "relational formula: associativity and nesting" =
-  rel_formula "(p && q) || (r && s)";
-  [%expect {| (Or (And (Ap p) (Ap q)) (And (Ap r) (Ap s))) |}];
-  rel_formula "mu X. (p && <a>X) || nu Y. (q && [b]Y)";
-  [%expect
-    {|
-    (Mu X
-     (Or (And (Ap p) (Diamond a () (Ap X)))
-      (Nu Y (And (Ap q) (Box b () (Ap Y))))))
-    |}];
-  rel_formula "p && q && r";
-  [%expect {| (And (And (Ap p) (Ap q)) (Ap r)) |}];
-  rel_formula "p || q || r";
-  [%expect {| (Or (Or (Ap p) (Ap q)) (Ap r)) |}];
-  rel_formula "p && (q && r)";
-  [%expect {| (And (Ap p) (And (Ap q) (Ap r))) |}]
+let test_rel_formula_associativity _ =
+  assert_sexp ~expected:"(Or (And (Ap p) (Ap q)) (And (Ap r) (Ap s)))"
+    (rel_formula "(p && q) || (r && s)");
+  assert_sexp
+    ~expected:
+      "(Mu X (Or (And (Ap p) (Diamond a () (Var X))) (Nu Y (And (Ap q) (Box b \
+       () (Var Y))))))"
+    (rel_formula "mu X. (p && <a>X) || nu Y. (q && [b]Y)");
+  assert_sexp ~expected:"(And (And (Ap p) (Ap q)) (Ap r))"
+    (rel_formula "p && q && r");
+  assert_sexp ~expected:"(Or (Or (Ap p) (Ap q)) (Ap r))"
+    (rel_formula "p || q || r");
+  assert_sexp ~expected:"(And (Ap p) (And (Ap q) (Ap r)))"
+    (rel_formula "p && (q && r)")
 
-let%expect_test "relational formula: mixed Unicode and ASCII" =
-  rel_formula "μ X. p ∧ <a>X || ν Y. q ∨ [b]Y";
-  [%expect
-    {|
-    (Mu X
-     (And (Ap p) (Diamond a () (Or (Ap X) (Nu Y (Or (Ap q) (Box b () (Ap Y))))))))
-    |}]
+let test_rel_formula_mixed_unicode _ =
+  assert_sexp
+    ~expected:
+      "(Mu X (And (Ap p) (Diamond a () (Or (Var X) (Nu Y (Or (Ap q) (Box b () \
+       (Var Y))))))))"
+    (rel_formula "μ X. p ∧ <a>X || ν Y. q ∨ [b]Y")
 
 (* ============================================================================ *)
 (* Relational Models *)
 (* ============================================================================ *)
 
-let%expect_test "relational model: parsing" =
-  rel_model "[s0: ({p}, [a: {s1, s2}])]";
-  [%expect {| ((s0 ((p) ((a (s1 s2)))))) |}];
-  rel_model "[]";
-  [%expect {| () |}];
-  rel_model "[s0: ({p, q}, [a: {s1}]), s1: ({r}, [])]";
-  [%expect {| ((s0 ((p q) ((a (s1))))) (s1 ((r) ()))) |}];
-  rel_model "[s0: ({p}, [a: {s1}, b: {s2, s3}])]";
-  [%expect {| ((s0 ((p) ((a (s1)) (b (s2 s3)))))) |}];
-  rel_model "[s0: ({p}, [])]";
-  [%expect {| ((s0 ((p) ()))) |}]
+let test_rel_model_parsing _ =
+  assert_sexp ~expected:"((s0 ((p) ((a (s1 s2))))))"
+    (rel_model "[s0: ({p}, [a: {s1, s2}])]");
+  assert_sexp ~expected:"()" (rel_model "[]");
+  assert_sexp ~expected:"((s0 ((p q) ((a (s1))))) (s1 ((r) ())))"
+    (rel_model "[s0: ({p, q}, [a: {s1}]), s1: ({r}, [])]");
+  assert_sexp ~expected:"((s0 ((p) ((a (s1)) (b (s2 s3))))))"
+    (rel_model "[s0: ({p}, [a: {s1}, b: {s2, s3}])]");
+  assert_sexp ~expected:"((s0 ((p) ())))" (rel_model "[s0: ({p}, [])]")
 
 (* ============================================================================ *)
 (* Probabilistic Models *)
 (* ============================================================================ *)
 
-let%expect_test "probabilistic model: parsing" =
-  prob_model "[s0: ({p}, [a: [s1: 1/2, s2: 1/2]])]";
-  [%expect {| ((s0 ((p) ((a ((s1 (1 2)) (s2 (1 2)))))))) |}];
-  prob_model "[]";
-  [%expect {| () |}];
-  prob_model "[s0: ({p}, [a: [s1: 1/1]])]";
-  [%expect {| ((s0 ((p) ((a ((s1 (1 1)))))))) |}];
-  prob_model "[s0: ({p}, [a: [s1: 3/4], b: [s2: 1/3]])]";
-  [%expect {| ((s0 ((p) ((a ((s1 (3 4)))) (b ((s2 (1 3)))))))) |}]
+let test_prob_model_parsing _ =
+  assert_sexp ~expected:"((s0 ((p) ((a ((s1 (1 2)) (s2 (1 2))))))))"
+    (prob_model "[s0: ({p}, [a: [s1: 1/2, s2: 1/2]])]");
+  assert_sexp ~expected:"()" (prob_model "[]");
+  assert_sexp ~expected:"((s0 ((p) ((a ((s1 (1 1))))))))"
+    (prob_model "[s0: ({p}, [a: [s1: 1/1]])]");
+  assert_sexp ~expected:"((s0 ((p) ((a ((s1 (3 4)))) (b ((s2 (1 3))))))))"
+    (prob_model "[s0: ({p}, [a: [s1: 3/4], b: [s2: 1/3]])]")
 
 (* ============================================================================ *)
 (* Probabilistic Formulas *)
 (* ============================================================================ *)
 
-let%expect_test "probabilistic formula: literals" =
-  prob_formula "true";
-  [%expect {| True |}];
-  prob_formula "false";
-  [%expect {| False |}];
-  prob_formula "p";
-  [%expect {| (Ap p) |}]
+let test_prob_formula_literals _ =
+  assert_sexp ~expected:"True" (prob_formula "true");
+  assert_sexp ~expected:"False" (prob_formula "false");
+  assert_sexp ~expected:"(Ap p)" (prob_formula "p")
 
-let%expect_test "probabilistic formula: negation" =
-  prob_formula "~p";
-  [%expect {| (Not (Ap p)) |}]
+let test_prob_formula_negation _ =
+  assert_sexp ~expected:"(Not (Ap p))" (prob_formula "~p")
 
-let%expect_test "probabilistic formula: connectives" =
-  prob_formula "p && q";
-  [%expect {| (And (Ap p) (Ap q)) |}];
-  prob_formula "p || q";
-  [%expect {| (Or (Ap p) (Ap q)) |}]
+let test_prob_formula_connectives _ =
+  assert_sexp ~expected:"(And (Ap p) (Ap q))" (prob_formula "p && q");
+  assert_sexp ~expected:"(Or (Ap p) (Ap q))" (prob_formula "p || q")
 
-let%expect_test "probabilistic formula: modalities with thresholds" =
-  prob_formula "<1/2> p";
-  [%expect {| (Diamond "" (1 2) (Ap p)) |}];
-  prob_formula "[1/4] p";
-  [%expect {| (Box "" (1 4) (Ap p)) |}];
-  prob_formula "<1/2 a> p";
-  [%expect {| (Diamond a (1 2) (Ap p)) |}];
-  prob_formula "[3/4 a] p";
-  [%expect {| (Box a (3 4) (Ap p)) |}]
+let test_prob_formula_modalities _ =
+  assert_sexp ~expected:{|(Diamond "" (1 2) (Ap p))|} (prob_formula "<1/2> p");
+  assert_sexp ~expected:{|(Box "" (1 4) (Ap p))|} (prob_formula "[1/4] p");
+  assert_sexp ~expected:"(Diamond a (1 2) (Ap p))" (prob_formula "<1/2 a> p");
+  assert_sexp ~expected:"(Box a (3 4) (Ap p))" (prob_formula "[3/4 a] p")
 
-let%expect_test "probabilistic formula: fixpoints" =
-  prob_formula "mu X. p || <1/2>X";
-  [%expect {| (Mu X (Or (Ap p) (Diamond "" (1 2) (Ap X)))) |}];
-  prob_formula "nu X. p && [1/2]X";
-  [%expect {| (Nu X (And (Ap p) (Box "" (1 2) (Ap X)))) |}]
+let test_prob_formula_fixpoints _ =
+  assert_sexp ~expected:{|(Mu X (Or (Ap p) (Diamond "" (1 2) (Var X))))|}
+    (prob_formula "mu X. p || <1/2>X");
+  assert_sexp ~expected:{|(Nu X (And (Ap p) (Box "" (1 2) (Var X))))|}
+    (prob_formula "nu X. p && [1/2]X")
 
-let%expect_test "probabilistic formula: complex and Unicode" =
-  prob_formula "mu X. (p && <1/2>X) || nu Y. (q && [1/3]Y)";
-  [%expect
-    {|
-    (Mu X
-     (Or (And (Ap p) (Diamond "" (1 2) (Ap X)))
-      (Nu Y (And (Ap q) (Box "" (1 3) (Ap Y))))))
-    |}];
-  prob_formula "μ X. p ∧ <1/2>X";
-  [%expect {| (Mu X (And (Ap p) (Diamond "" (1 2) (Ap X)))) |}]
+let test_prob_formula_complex _ =
+  assert_sexp
+    ~expected:
+      {|(Mu X (Or (And (Ap p) (Diamond "" (1 2) (Var X))) (Nu Y (And (Ap q) (Box "" (1 3) (Var Y))))))|}
+    (prob_formula "mu X. (p && <1/2>X) || nu Y. (q && [1/3]Y)");
+  assert_sexp ~expected:{|(Mu X (And (Ap p) (Diamond "" (1 2) (Var X))))|}
+    (prob_formula "μ X. p ∧ <1/2>X")
 
 (* ============================================================================ *)
-(* Logics Module Tests *)
+(* NNF conversion *)
 (* ============================================================================ *)
 
-let%expect_test "Logics.Relational: parse formula and model" =
-  logics_rel_formula "<a>p";
-  [%expect {| (Diamond a () (Ap p)) |}];
-  logics_rel_model "[s0: ({p}, [a: {s1}])]";
-  [%expect {| ((s0 ((p) ((a (s1)))))) |}]
+let test_prob_nnf _ =
+  assert_sexp ~expected:"(Or (Not p) (Not q))" (logics_prob_to_nnf "~(p && q)");
+  assert_sexp ~expected:{|(Box "" (1 2) (Not p))|}
+    (logics_prob_to_nnf "~<1/2>p");
+  assert_sexp
+    ~expected:
+      {|(Mu X (Or (And (Ap p) (Diamond "" (1 2) (Var X))) (Nu Y (And (Ap q) (Box "" (1 3) (Var Y))))))|}
+    (logics_prob_to_nnf "mu X. (p && <1/2>X) || nu Y. (q && [1/3]Y)")
 
-let%expect_test "Logics.Probabilistic: parse formula" =
-  logics_prob_formula "<1/2>p";
-  [%expect {| (Diamond "" (1 2) (Ap p)) |}];
-  logics_prob_formula "[1/3]p";
-  [%expect {| (Box "" (1 3) (Ap p)) |}];
-  logics_prob_formula "mu X. p || <1/2>X";
-  [%expect {| (Mu X (Or (Ap p) (Diamond "" (1 2) (Ap X)))) |}];
-  logics_prob_formula "nu X. p && [3/4]X";
-  [%expect {| (Nu X (And (Ap p) (Box "" (3 4) (Ap X)))) |}]
+(* ============================================================================ *)
+(* Suite *)
+(* ============================================================================ *)
 
-let%expect_test "Logics.Probabilistic: parse model" =
-  logics_prob_model "[s0: ({p}, [a: [s1: 1/2, s2: 1/2]])]";
-  [%expect {| ((s0 ((p) ((a ((s1 (1 2)) (s2 (1 2)))))))) |}]
+let suite =
+  "Parsing"
+  >::: [
+         "Relational formulas"
+         >::: [
+                "literals" >:: test_rel_formula_literals;
+                "negation" >:: test_rel_formula_negation;
+                "conjunction" >:: test_rel_formula_conjunction;
+                "disjunction" >:: test_rel_formula_disjunction;
+                "modalities" >:: test_rel_formula_modalities;
+                "fixpoints" >:: test_rel_formula_fixpoints;
+                "associativity" >:: test_rel_formula_associativity;
+                "mixed Unicode" >:: test_rel_formula_mixed_unicode;
+              ];
+         "Relational models" >::: [ "parsing" >:: test_rel_model_parsing ];
+         "Probabilistic formulas"
+         >::: [
+                "literals" >:: test_prob_formula_literals;
+                "negation" >:: test_prob_formula_negation;
+                "connectives" >:: test_prob_formula_connectives;
+                "modalities" >:: test_prob_formula_modalities;
+                "fixpoints" >:: test_prob_formula_fixpoints;
+                "complex" >:: test_prob_formula_complex;
+              ];
+         "Probabilistic models" >::: [ "parsing" >:: test_prob_model_parsing ];
+         "NNF conversion" >::: [ "probabilistic" >:: test_prob_nnf ];
+       ]
 
-let%expect_test "Logics.Probabilistic: formula_of_ast (NNF conversion)" =
-  logics_prob_to_nnf "~(p && q)";
-  [%expect {| (Or (Not p) (Not q)) |}];
-  logics_prob_to_nnf "~<1/2>p";
-  [%expect {| (Box "" (1 2) (Not p)) |}];
-  logics_prob_to_nnf "mu X. (p && <1/2>X) || nu Y. (q && [1/3]Y)";
-  [%expect
-    {|
-    (Mu X
-     (Or (And (Ap p) (Diamond "" (1 2) (Ap X)))
-      (Nu Y (And (Ap q) (Box "" (1 3) (Ap Y))))))
-    |}]
+let () = run_test_tt_main suite
